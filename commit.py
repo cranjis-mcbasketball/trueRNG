a8188d1ada94df2399009c3bdc2fd985722b30db
Mason Borchard <masonborchard@gmail.com>
2020-09-18 19:30:49 -0700

    added pull-changes.py script to update repo in aws ec2 ubuntu

--- commit.py
+++ commit.py
@@ -1,13 +1,11 @@
-
 import subprocess as cmd
 import time
 from datetime import datetime
 
 
-
 def updateGithub():
     while True:
-        
+
         cmd.run("git add NED_BYTES", check=True, shell=True)
         cmd.run(f"git commit -m 'NED_BYTES'", check=True, shell=True)
         cmd.run("git push origin master", check=True, shell=True)
