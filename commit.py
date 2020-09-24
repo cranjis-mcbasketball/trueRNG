24f4530f31a7ae3c6acb48479c1bd2fcc4eb4fed
Mason Borchard <masonborchard@gmail.com>
2020-09-24 14:21:54 -0700

    'NED_BYTES'

--- commit.py
+++ commit.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def updateGithub():
     while True:
 
-        cmd.run("git add NED_BYTES", check=True, shell=True)
+        cmd.run("git add .", check=True, shell=True)
         cmd.run(f"git commit -m 'NED_BYTES'", check=True, shell=True)
         cmd.run("git push origin master", check=True, shell=True)
         now = datetime.now()
