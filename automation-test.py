b3d45c9e3cbb82785e2d117627ce42d21a67c2e1
Mason Borchard <masonborchard@gmail.com>
2020-09-24 16:56:42 -0700

    'NED_BYTES'

--- automation-test.py
+++ automation-test.py
@@ -1,2 +1,3 @@
 commit1 = "commit1"
 commit2 = "commit2"
+commit 3 = "commit 3"
--- commit.py
+++ commit.py
@@ -2,10 +2,14 @@ import subprocess as cmd
 import time
 from datetime import datetime
 
+n = 1
+
 
 def updateGithub():
     while True:
         cmd.run("git add .", check=True, shell=True)
+        global n
+        global_n = n
         cmd.run(f"git commit -m 'NED_BYTES'", check=True, shell=True)
         cmd.run("git push origin master", check=True, shell=True)
         now = datetime.now()
@@ -13,7 +17,9 @@ def updateGithub():
         cmd.run("git reset --hard", check=True, shell=True)
         now = datetime.now()
         print('completed reset: ', now)
+        n = n + 1
         time.sleep(600)
+        print("%d squared is %d" % (10, 10*10))
 
 
 updateGithub()
